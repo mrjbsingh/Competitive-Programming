da669730be821c9f04b36f8595e5795e247dc550
mrjbsingh <jayendra.iiita@gmail.com>
2022-03-07 17:17:51 +0530

    code for modular Inverse using GCDmethod

--- CodeTemplate.java
+++ CodeTemplate.java
@@ -151,4 +151,36 @@ public class CodeTemplate{
         }
         return primeNum;
     }
+
+    public long modInverse(long a, long m)
+    {
+        long m0 = m;
+        long y = 0, x = 1;
+ 
+        if (m == 1)
+            return 0;
+ 
+        while (a > 1) {
+            // q is quotient
+            long q = a / m;
+ 
+            long t = m;
+ 
+            // m is remainder now, process
+            // same as Euclid's algo
+            m = a % m;
+            a = t;
+            t = y;
+ 
+            // Update x and y
+            y = x - q * y;
+            x = t;
+        }
+ 
+        // Make x positive
+        if (x < 0)
+            x += m0;
+ 
+        return x;
+    }
 }
