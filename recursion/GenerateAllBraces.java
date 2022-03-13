7e8dc6695f73f8eeb8d4ec8f1764923ee7f9b989
mrjbsingh <jayendra.iiita@gmail.com>
2022-03-13 18:39:57 +0530

    good qst to generate all combination of paranthesis

---
+++ recursion/GenerateAllBraces.java
@@ -0,0 +1,38 @@
+package recursion;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class GenerateAllBraces{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        
+        GenerateAllBraces pm = new GenerateAllBraces();
+        ArrayList<String> res = pm.generateParenthesis(A);
+        for (int i = 0; i < res.size(); i++) {
+            System.out.println(res.get(i));
+        }
+    }
+    public ArrayList<String> generateParenthesis(int A) {
+        ArrayList<String> res = new ArrayList<>();
+        solve(A, 0, A*2, "", res);
+        return res;
+    }
+    void solve(int count, int diff, int len, String combination, ArrayList<String> res){
+        if(len == 0){
+            res.add(combination);
+            return ;
+        }
+        if(count>0){
+            solve(count-1, diff+1, len-1, combination+"(", res);
+        }
+        if(diff>0){
+            solve(count, diff-1, len-1, combination+")", res);
+        }
+        
+    }
+
+   
+}
