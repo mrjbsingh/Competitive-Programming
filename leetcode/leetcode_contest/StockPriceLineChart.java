56f9983bcbe60071398291a84a9a7b555ccf7d96
mrjbsingh <jayendra.iiita@gmail.com>
2022-05-22 10:17:57 +0530

    good qst tricky as slope calculation can go wrong in doubles

---
+++ leetcode/leetcode_contest/StockPriceLineChart.java
@@ -0,0 +1,46 @@
+package leetcode.leetcode_contest;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class StockPriceLineChart {
+    public int minimumLines(int[][] stockPrices) {
+        int n = stockPrices.length;
+        if(n==1)
+            return 0;
+        Arrays.sort(stockPrices, new Sort2dArr(0));
+        long previousY = (long)stockPrices[1][1]- (long)stockPrices[0][1];
+        long previousX = (long)stockPrices[1][0]- (long)stockPrices[0][0];
+        //Double lastSlope = y/x;
+        int count=1;
+        if(n==2)
+            return count;
+        for(int i=2; i<n; i++){
+            //System.out.print(stockPrices[i][0]+" ");
+            long y = (long)stockPrices[i][1]- (long)stockPrices[i-1][1];
+            long x = (long)stockPrices[i][0]- (long)stockPrices[i-1][0];
+
+            if(previousY*x !=previousX*y){
+                //System.out.print(" inside ");
+                count++;
+                previousY = y;
+                previousX = x;
+            }
+
+        }
+        return count;
+    }
+    public class Sort2dArr implements Comparator<int []> {
+        int column;
+        Sort2dArr(int column){
+            this.column = column;
+        }
+        public int compare(final int entry1[], final int entry2[]){
+            if(entry1[column]>= entry2[column]){
+                return 1;
+            }
+            else
+                return -1;
+        }
+    }
+}
